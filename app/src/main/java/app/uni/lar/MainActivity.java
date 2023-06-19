d837daf231ccd86c919fb632f570145ac1b68892
larestan1381 <larestan1381@gmail.com>
2023-06-19 18:43:58 +0430

    add splash creen

--- app/src/main/java/app/uni/lar/MainActivity.java
+++ app/src/main/java/app/uni/lar/MainActivity.java
@@ -2,7 +2,10 @@ package app.uni.lar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final int splashTimeOut = 3000;
+
+        Thread splashThread = new Thread(){
+            int wait = 0;
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    while(wait < splashTimeOut){
+                        sleep(100);
+                        wait += 100;
+                    }
+                } catch (Exception e) {
+                }finally{
+                    startActivity(new Intent(,MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        splashThread.start();
     }
 }
